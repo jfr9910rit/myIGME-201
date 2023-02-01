7302edc4fa0350890299dd650e6ea7887bc7beb4
Julian Reester <jreester@gmail.com>
2023-01-31 19:09:20 -0500

    pe4 part 1

---
+++ pe4part1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe4part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1, var2;
+            Console.WriteLine("Enter Two Numbers ");
+            var1 = Convert.ToInt32(Console.ReadLine());
+            var2 = Convert.ToInt32(Console.ReadLine());
+            if (var1 >= 10 && var2 >= 10)
+            {
+                Console.WriteLine("Invalid Input ");
+            }
+            else if (var1 <= 10 && var2 <= 10)
+            {
+                Console.WriteLine("Invalid Input ");
+            }
+            else if (var1 > 1)
+            {
+                Console.WriteLine("Valid Input ");
+            }
+            else if (var2 > 1)
+            {
+                Console.WriteLine("Valid Input ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
