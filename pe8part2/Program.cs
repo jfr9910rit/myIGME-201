c887cd688cf031640e88bdd0bbcd44ee27594584
Julian Reester <jreester@gmail.com>
2023-02-08 22:30:58 -0500

    pe8 part2

---
+++ pe8part2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe8part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write a string: ");
+            string str = Console.ReadLine();
+            string str2 = "";
+            int len = str.Length -1;
+            for(int i = len; i > 0; i--)
+            {
+                str2 = str2 + str[i];
+            }
+            str2 += str[0];
+            Console.WriteLine("The reverse string is: " + str2);
+        }
+    }
+}
