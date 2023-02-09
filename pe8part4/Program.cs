11a333268656361539d31491aeac4d1a70a2a83e
Julian Reester <jreester@gmail.com>
2023-02-08 23:00:16 -0500

    pe8 part 4

---
+++ pe8part4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe8part4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+            var x = input.Split(' ').Select(a => "\"" + a + "\"");
+            foreach (var s in x)
+            {
+                Console.WriteLine(s);
+            }
+           
+        }
+    }
+}
