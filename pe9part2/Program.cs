f0703382ff51c90941aae74e665bbb29f2b0ef5f
Julian Reester <jreester@gmail.com>
2023-02-14 23:46:42 -0500

    pe9part2

---
+++ pe9part2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe9part2
+{
+    internal class Program
+    {
+
+        delegate string Reader();
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader(ReadText);
+            Console.WriteLine("Type below");
+            Console.WriteLine();
+            string test = reader();
+            Console.WriteLine(test);
+        }
+
+        public static string ReadText()
+        {
+            return Console.ReadLine();
+
+        }
+    }
+}
