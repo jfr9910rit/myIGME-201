60c72ac16e99c5a8f491437488ab655a508d249d
Julian Reester <jreester@gmail.com>
2023-03-07 20:00:22 -0500

    pe11 q5

---
+++ PE11Part1_Reester/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE11Part1_Reester
+{
+    abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+    }
+
+    abstract class Car : Vehicle
+    {
+
+    }
+
+    abstract class Train : Vehicle
+    {
+
+    }
+
+    class Compact : Car, IPassengerCarrier
+    {
+
+    }
+
+    class SUV : Car, IPassengerCarrier
+    {
+
+    }
+
+    class Pickup : Car, IPassengerCarrier
+    {
+
+    }
+
+    class PassengerTrain : Train, IPassengerCarrier
+    {
+
+    }
+
+    class FreightTrain : Train, IHeavyLoadCarrier
+    {
+
+    }
+
+    class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+
+    }
+
+    interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
+
+    interface IHeavyLoadCarrier
+    {
+
+    }
+}
