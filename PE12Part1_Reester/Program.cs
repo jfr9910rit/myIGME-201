936c1274ec4794c8fef738c03d8ad83e32d2fedd
Julian Reester <jreester@gmail.com>
2023-03-07 22:28:44 -0500

    pe12 q1

---
+++ PE12Part1_Reester/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12Part1_Reester
+{
+    public class MyClass
+    {
+        string myString;
+        string MyString
+        {
+            set { myString = value; }
+        }
+        public virtual string GetString()
+        {
+            return myString;
+        }
+    }
+
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            // Call the base implementation of GetString() and append some text to the returned string
+            return base.GetString() + " output from the derived class.";
+        }
+    }
+
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
