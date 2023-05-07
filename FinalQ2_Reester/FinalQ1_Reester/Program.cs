ddf2519cad4fa3864353c455765cfe5f87dd2cd4
Julian Reester <jreester@gmail.com>
2023-05-07 19:22:43 -0400

    final question 2

---
+++ FinalQ2_Reester/FinalQ1_Reester/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalQ1_Reester
+{
+    internal class Program
+    {
+        private List<int> list = new List<int>();
+
+        public void Push(int n)
+        {
+            list.Add(n);
+        }
+
+        public int Pop()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            int lastIndex = list.Count - 1;
+            int lastItem = list[lastIndex];
+            list.RemoveAt(lastIndex);
+            return lastItem;
+        }
+
+        public int Peek()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return list[list.Count - 1];
+        }
+    }
+}
---
+++ FinalQ2_Reester/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalQ1_Reester
+{
+    class MyQueue
+    {
+        private List<int> elements;
+
+        public MyQueue()
+        {
+            elements = new List<int>();
+        }
+
+        public void Enqueue(int n)
+        {
+            elements.Add(n);
+        }
+
+        public int Dequeue()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            int firstElement = elements[0];
+            elements.RemoveAt(0);
+            return firstElement;
+        }
+
+        public int Peek()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return elements[0];
+        }
+    }
+
+}
+
