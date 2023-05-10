0e50a3d82fdbc8e7437f17ba835877307334d4f2
Julian Reester <jreester@gmail.com>
2023-05-10 04:37:20 -0400

    Goals Page with list functionally for completing and removing goals

---
+++ GoalsPage/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace GoalsPage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.goalsList = new System.Windows.Forms.ListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.goalTextBox = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.completeButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Workout App";
+            // 
+            // goalsList
+            // 
+            this.goalsList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goalsList.FormattingEnabled = true;
+            this.goalsList.ItemHeight = 25;
+            this.goalsList.Location = new System.Drawing.Point(55, 444);
+            this.goalsList.Name = "goalsList";
+            this.goalsList.Size = new System.Drawing.Size(762, 379);
+            this.goalsList.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(695, 239);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(113, 71);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add Goal";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // goalTextBox
+            // 
+            this.goalTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goalTextBox.Location = new System.Drawing.Point(91, 222);
+            this.goalTextBox.Name = "goalTextBox";
+            this.goalTextBox.Size = new System.Drawing.Size(575, 117);
+            this.goalTextBox.TabIndex = 3;
+            this.goalTextBox.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::GoalsPage.Properties.Resources.logoDraftSm;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(33, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 150);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // completeButton
+            // 
+            this.completeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.completeButton.Location = new System.Drawing.Point(64, 844);
+            this.completeButton.Name = "completeButton";
+            this.completeButton.Size = new System.Drawing.Size(188, 53);
+            this.completeButton.TabIndex = 5;
+            this.completeButton.Text = "Complete";
+            this.completeButton.UseVisualStyleBackColor = true;
+            this.completeButton.Click += new System.EventHandler(this.completeButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeButton.Location = new System.Drawing.Point(679, 844);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(138, 53);
+            this.removeButton.TabIndex = 6;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(243, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter Goal Below";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(351, 844);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 52);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "View Workout";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 933);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.completeButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.goalTextBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.goalsList);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox goalsList;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.RichTextBox goalTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button completeButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ GoalsPage/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoalsPage
+{
+    public partial class Form1 : Form
+    {
+        private List<string> goals = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Workout App Goals";
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string goal = goalTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(goal))
+            {
+                goals.Add(goal);
+                goalTextBox.Clear();
+                UpdateGoalsList();
+            }
+        }
+
+        private void completeButton_Click(object sender, EventArgs e)
+        {
+            int index = goalsList.SelectedIndex;
+            if (index >= 0 && index < goals.Count)
+            {
+                goals[index] = "[Complete] " + goals[index];
+                UpdateGoalsList();
+            }
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            int index = goalsList.SelectedIndex;
+            if (index >= 0 && index < goals.Count)
+            {
+                goals.RemoveAt(index);
+                UpdateGoalsList();
+            }
+        }
+
+        private void UpdateGoalsList()
+        {
+            goalsList.Items.Clear();
+            foreach (string goal in goals)
+            {
+                goalsList.Items.Add(goal);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //go to home page
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            int index = goalsList.SelectedIndex;
+            if (index >= 0 && index < goals.Count)
+            {
+                //go to workout page
+                //at goals[index];
+            }
+        }
+
+        
+    }
+}
